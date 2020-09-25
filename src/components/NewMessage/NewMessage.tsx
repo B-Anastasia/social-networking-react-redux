8695358262fb71c6802b7739f94b25c8ca9d258e
Anastasia <boshniak.a@gmail.com>
2020-09-25 23:28:53 +0300

    -076- redux-form (add message in Dialog component)

--- src/components/NewMessage/NewMessage.tsx
+++ src/components/NewMessage/NewMessage.tsx
@@ -1,14 +1,33 @@
 import React, {ChangeEvent} from "react";
 import {INewMessageType,} from "../../redux/store";
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
 type INewMessagePropsType = {
     newMessageText: string
     onClickAddNewMessage: (newMessage: INewMessageType) => void
     onChangeInputValue: (newValue: string) => void
 };
+type IFieldsType={
+    newMessageBody:string
+}
+type IOwnProps ={
+}
 
-function NewMessage({newMessageText, onClickAddNewMessage, onChangeInputValue}: INewMessagePropsType) {
 
+const AddMessageForm:React.FC<InjectedFormProps<IFieldsType> & IOwnProps > =(props)=>{
+    return(
+         <form onSubmit={props.handleSubmit}>
+             <Field component={'textarea'} name={'newMessageBody'} placeholder={"New message text"}/>
+        <button>Send</button>
+    </form>
+    )
+}
+const AddMessageFormRedux=reduxForm<IFieldsType,IOwnProps>({
+    form:'dialogAddMessageForm'}
+    )(AddMessageForm)
+
+function NewMessage(props: INewMessagePropsType) {
+const { onClickAddNewMessage, onChangeInputValue}=props;
     const message = React.createRef<HTMLTextAreaElement>();
 
     const onClickAddNewMessageHandler = () => {
@@ -24,22 +43,20 @@ function NewMessage({newMessageText, onClickAddNewMessage, onChangeInputValue}: 
         }
     };
 
+    const onSubmitAddNewMessageHandler=(data:IFieldsType)=>{
+        console.log(data)
+    }
+
     const onChangeInputValueHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
         onChangeInputValue(e.currentTarget.value);
     };
 
     return (
         <div>
-      <textarea
-          value={newMessageText}
-          ref={message}
-          name="message"
-          placeholder={"New"}
-          onChange={onChangeInputValueHandler}
-      />
-            <button onClick={onClickAddNewMessageHandler}>Add</button>
+            <AddMessageFormRedux onSubmit={onSubmitAddNewMessageHandler}/>
         </div>
     );
 }
 
 export default NewMessage;
+
